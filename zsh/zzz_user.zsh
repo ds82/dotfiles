9d9a10b747b31ee04687314e4047a698eaf01f99
Dennis Sänger <mail@dennis.io>
2014-11-10 11:13:54 +0100

    updated home zsh config

---
+++ zsh/zzz_user.zsh
@@ -0,0 +1,26 @@
+export PATH=/usr/local/bin:$PATH
+
+## fix alt cursor keys in console
+bindkey -e
+
+bindkey '[C' forward-word
+bindkey '[D' backward-word
+
+#bindkey "^[[1;5C" forward-word
+#bindkey "^[[1;5D" backward-word
+
+setopt no_share_history
+
+export GIT_EDITOR='vim'
+
+fixssh() {
+  for key in SSH_AUTH_SOCK SSH_CONNECTION SSH_CLIENT; do
+    if (tmux show-environment | grep "^${key}" > /dev/null); then
+      value=`tmux show-environment | grep "^${key}" | sed -e "s/^[A-Z_]*=//"`
+      export ${key}="${value}"
+    fi
+  done
+}
+
+## docker
+export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375

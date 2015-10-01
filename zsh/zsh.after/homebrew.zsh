22430fc4c09af1b594e27b2ac66fe199d26e63c1
Dennis Sänger <mail@dennis.io>
2015-10-01 18:45:26 +0200

    feat: read GITHUB token only if available

--- zsh/zsh.after/homebrew.zsh
+++ zsh/zsh.after/homebrew.zsh
@@ -1,3 +1,5 @@
-TOKEN=$(cat ~/.github-token)
-export HOMEBREW_GITHUB_API_TOKEN=$TOKEN
+if [[ -e ~/.github-token ]]; then
+  TOKEN=$(cat ~/.github-token)
+  export HOMEBREW_GITHUB_API_TOKEN=$TOKEN
+fi
 alias bru='brew update && brew upgrade --all'

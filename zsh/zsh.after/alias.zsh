e2a2a7f752a0beccbc87e27680d4cbf5b5650693
Dennis Sänger <mail@dennis.io>
2015-10-01 18:08:11 +0200

    feat: put zsh.after scripts into repo

---
+++ zsh/zsh.after/alias.zsh
@@ -0,0 +1,4 @@
+alias vmrun="/Applications/VMware\ Fusion.app/Contents/Library/vmrun"
+alias bru="brew update && brew upgrade --all"
+alias sleepnow='sudo pmset sleepnow && exit'
+
---
+++ zsh/zsh.after/env.zsh
@@ -0,0 +1,1 @@
+export HGS="/Users/dennis/code/spiceadm"

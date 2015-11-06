efd2b0df3ef76c6f252b0fe9c384d7a943c03c4c
Dennis Sänger <mail@dennis.io>
2015-11-06 09:04:45 +0100

    feat(alias): add dns-clear alias

--- zsh/zsh.after/alias.zsh
+++ zsh/zsh.after/alias.zsh
@@ -2,3 +2,4 @@ alias vmrun="/Applications/VMware\ Fusion.app/Contents/Library/vmrun"
 alias bru="brew update && brew upgrade --all"
 alias sleepnow='sudo pmset sleepnow && exit'
 
+alias dns-clear="sudo discoveryutil mdnsflushcache;sudo discoveryutil udnsflushcaches;say flushed"

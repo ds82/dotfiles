9a8790d11a18d11583b99794af3e3958556df9fe
Dennis Sänger <mail@dennis.io>
2015-12-18 11:19:22 +0100

    feat(dns-clear): alias to clear OSX dns cache

--- zsh/zsh.after/alias.zsh
+++ zsh/zsh.after/alias.zsh
@@ -2,4 +2,4 @@ alias vmrun="/Applications/VMware\ Fusion.app/Contents/Library/vmrun"
 alias bru="brew update && brew upgrade --all"
 alias sleepnow='sudo pmset sleepnow && exit'
 
-alias dns-clear="sudo discoveryutil mdnsflushcache;sudo discoveryutil udnsflushcaches;say flushed"
+alias dns-clear="sudo killall -HUP mDNSResponder; sudo discoveryutil mdnsflushcache;sudo discoveryutil udnsflushcaches"

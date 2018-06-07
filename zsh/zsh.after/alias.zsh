b94468eba5106681b869e2cda110103a1da62740
Dennis Sänger <mail@dennis.io>
2018-06-07 09:11:31 +0200

    feat: add `scan` alias for work

--- zsh/zsh.after/alias.zsh
+++ zsh/zsh.after/alias.zsh
@@ -3,3 +3,5 @@ alias bru="brew update && brew upgrade --all"
 alias sleepnow='sudo pmset sleepnow && exit'
 
 alias dns-clear="sudo killall -HUP mDNSResponder; sudo discoveryutil mdnsflushcache;sudo discoveryutil udnsflushcaches"
+
+alias scan='pushd ~/code/ssdp-discover; ./node_modules/.bin/ts-node scan.ts; popd'

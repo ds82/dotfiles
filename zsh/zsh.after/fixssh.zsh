61911653026fc2bc9285cc9cd247e905d4abf2bb
Dennis Sänger <mail@dennis.io>
2018-06-07 09:08:00 +0200

    feat: alias to ssh-add -K all my keys

--- zsh/zsh.after/fixssh.zsh
+++ zsh/zsh.after/fixssh.zsh
@@ -1,3 +1,7 @@
+addssh() {
+  ssh-add -K ~/.ssh/rea; ssh-add -K ~/.ssh/id_rsa; ssh-add -K ~/.ssh/bd_rsa
+}
+
 fixssh() {
   for key in SSH_AUTH_SOCK SSH_CONNECTION SSH_CLIENT; do
     if (tmux show-environment | grep "^${key}" > /dev/null); then

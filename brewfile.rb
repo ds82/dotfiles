a395da3f12bfe4f907bcbf556938499b28c0baf3
Dennis Sänger <mail@dennis.io>
2019-01-21 07:20:15 +0100

    feat(brew): first brewfile draft

---
+++ brewfile.rb
@@ -0,0 +1,42 @@
+tap 'caskroom/cask'
+tap 'caskroom/fonts'
+
+brew 'git'
+brew 'git-extras'
+brew 'ag'
+brew 'jq'
+brew 'fzf'
+brew 'tmux'
+brew 'tig'
+brew 'curl'
+brew 'nmap'
+brew 'tcpdump'
+brew 'entr'
+brew 'gawk'
+brew 'htop'
+brew 'midnight-commander'
+brew 'ncdu'
+brew 'rsync'
+brew 'tldr'
+brew 'zsh'
+
+brew 'vim', args: ['with-python3', 'with-lua', 'with-luajit']
+brew 'neovim'
+
+brew 'haskell-stack'
+
+brew 'mas'
+cask 'spotify'
+cask 'vlc'
+
+
+cask 'dash'
+cask 'alfred'
+cask 'docker'
+cask 'owncloud'
+cask 'colorpicker-skalacolor'
+cask 'appcleaner'
+cask 'franz'
+
+cask 'visual-studio-code'
+

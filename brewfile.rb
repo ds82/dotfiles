5a235dbf4e46a8b10cb61b4f78b59a291e942be3
Dennis Sänger <mail@dennis.io>
2019-08-20 12:34:20 +0200

    feat: add missing apps/tools in brewfile

--- brewfile.rb
+++ brewfile.rb
@@ -3,6 +3,7 @@ tap 'caskroom/fonts'
 
 brew 'git'
 brew 'git-extras'
+brew 'hub'
 brew 'ag'
 brew 'jq'
 brew 'fzf'
@@ -19,6 +20,9 @@ brew 'ncdu'
 brew 'rsync'
 brew 'tldr'
 brew 'zsh'
+brew 'iterm2'
+brew 'nvm'
+brew 'pandoc'
 
 brew 'vim', args: ['with-python3', 'with-lua', 'with-luajit']
 brew 'neovim'
@@ -39,4 +43,14 @@ cask 'appcleaner'
 cask 'franz'
 
 cask 'visual-studio-code'
+
+mas install 824183456   # Affinity Photo (1.7.2)
+mas install 824171161   # Affinity Designer (1.7.2)
+mas install 497799835   # Xcode (10.3)
+mas install 1054607607  # Helium (2.0)
+mas install 1384080005  # Tweetbot (3.3.1)
+mas install 470158793   # Keka (1.1.17)
+mas install 890031187   # Marked 2 (2.5.33)
+mas install 880001334   # Reeder (3.2.1)
+
 

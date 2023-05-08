bd64bbba6fe4112cb6105b578cb1352d3484b4ee
Dennis Sänger <git@mail.ds82.de>
2023-05-08 15:03:44 +0200

    feat(nvim): add ,ag search for visual selections

--- nvim/after/plugin/fzf.lua
+++ nvim/after/plugin/fzf.lua
@@ -1,3 +1,7 @@
 -- search word under cursor with fzf
 vim.keymap.set("n", "<leader>ag", ":Rg <C-R><C-W><CR>")
-
+vim.keymap.set("v", "<leader>ag", function()
+	vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg('v')
+	vim.cmd(':Rg ' .. text)
+end)

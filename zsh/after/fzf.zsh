7b4d471e9d5b272155af138653e0c79f401a6580
Dennis Sänger <git@mail.ds82.de>
2023-07-31 20:04:27 +0200

    feat(fzf): take current word when hit ESC

--- zsh/after/fzf.zsh
+++ zsh/after/fzf.zsh
@@ -1,6 +1,6 @@
 ## export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*,!node_modules/*"'
 export FZF_DEFAULT_COMMAND='rg --files --follow --hidden'
-export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history"
+export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history --bind esc:print-query"
 
 [[ -f "$HOME/.fzf.zsh" ]] && source "$HOME/.fzf.zsh"
 

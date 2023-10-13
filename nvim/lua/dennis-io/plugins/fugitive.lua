74ccdc0fc1faacd032574034cb2f1355f928f992
Dennis Sänger <git@mail.ds82.de>
2023-10-13 17:10:42 +0200

    fix(nvim): load fugitive on VimEnter

--- nvim/lua/dennis-io/plugins/fugitive.lua
+++ nvim/lua/dennis-io/plugins/fugitive.lua
@@ -1,10 +1,10 @@
 return {
-  "tpope/vim-fugitive",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-    vim.keymap.set("n", "<leader>grc", function()
-      vim.cmd(':G rebase --continue')
-    end)
-  end
+	"tpope/vim-fugitive",
+	event = { "VimEnter" },
+	config = function()
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>grc", function()
+			vim.cmd(":G rebase --continue")
+		end)
+	end,
 }

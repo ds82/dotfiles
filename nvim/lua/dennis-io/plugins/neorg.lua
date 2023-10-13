38fa858bc2d8e2acc9018002287343d20b683854
Dennis Sänger <git@mail.ds82.de>
2023-10-13 17:13:24 +0200

    fix(nvim): add missing neorg concealer config

--- nvim/lua/dennis-io/plugins/neorg.lua
+++ nvim/lua/dennis-io/plugins/neorg.lua
@@ -10,6 +10,7 @@ return {
 		require("neorg").setup({
 			load = {
 				["core.defaults"] = {}, -- Loads default behaviour
+				["core.concealer"] = {}, -- Adds pretty icons to your documents
 				["core.dirman"] = { -- Manages Neorg workspaces
 					config = {
 						workspaces = {

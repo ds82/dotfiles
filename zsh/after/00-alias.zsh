461646427ca4667e9271d7f58d252bab497d7e89
Dennis Sänger <git@mail.ds82.de>
2023-12-30 18:16:10 +0100

    fix(zsh/alias): check if binaries exist before aliasing them

--- zsh/after/00-alias.zsh
+++ zsh/after/00-alias.zsh
@@ -1,4 +1,7 @@
-alias bru="brew update && brew upgrade --all"
+if command -v brew &> /dev/null; then
+  alias bru="brew update && brew upgrade --all"
+fi
+
 alias sleepnow='sudo pmset sleepnow && exit'
 
 alias dns-clear="sudo killall -HUP mDNSResponder; sudo discoveryutil mdnsflushcache;sudo discoveryutil udnsflushcaches"
@@ -9,8 +12,16 @@ alias fix-perms='sudo chown -R dsaenger $WEB/node_modules $WEB/modules/*/package
 
 alias commit-cache="git add yarn.lock cache/ && git commit -m 'chore(lock/cache): update lock & cache'"
 
-alias ll='eza -a -l'
-alias ls='eza -l'
+if command -v eza ?> /dev/null; then
+  alias ll='eza -a -l'
+  alias ls='eza -l'
+fi
+
+# in linux fd is called fdfind; alias it
+if command -v fdfind &> /dev/null; then
+  alias fd=fdfind
+fi
+
 
 alias reflog='git reflog --pretty=raw | tig --pretty=raw'
 

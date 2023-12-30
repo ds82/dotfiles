1ba85e168aff6f40ccb61131cd71e12d3e009823
Dennis Sänger <git@mail.ds82.de>
2023-12-30 18:16:34 +0100

    fix(zsh): check if homebrew exists before exporting CC env var

--- zsh/after/cc.zsh
+++ zsh/after/cc.zsh
@@ -1,5 +1,8 @@
-CC=$(fd "^gcc-[0-9]+$" /opt/homebrew/bin)
+if [[ -d /opt/homebrew/bin ]]; then
+  CC=$(fd "^gcc-[0-9]+$" /opt/homebrew/bin)
 
-if [[ ! -z "$CC" ]]; then
-  export CC=${CC}
+  if [[ ! -z "$CC" ]]; then
+    export CC=${CC}
+  fi
 fi
+

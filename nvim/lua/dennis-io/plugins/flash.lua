cadd313c144fdb15ed3e3f96a28535d4ac593a6c
Dennis Sänger <git@mail.ds82.de>
2024-01-25 19:31:47 +0100

    feat(nvim/flash): re-enable and re-configure folke/flash

--- nvim/lua/dennis-io/plugins/flash.lua
+++ nvim/lua/dennis-io/plugins/flash.lua
@@ -1,21 +1,23 @@
 return {
-  "folke/flash.nvim",
-  event = "VeryLazy",
-  ---@type Flash.Config
-  opts = {
-    modes = {
-      search = {
-        enabled = false,
-        jump = { history = false, register = false, nohlsearch = false },
-      }
-    }
-  },
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {
+		search = {
+			multi_window = false,
+		},
+		modes = {
+			search = {
+				enabled = false,
+			},
+		},
+	},
   -- stylua: ignore
   keys = {
-    { "<leader>/", mode = { "n", "o" }, function() require("flash").jump() end,       desc = "Flash" },
-    { "<leader>?", mode = { "n", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "-", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "_", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     -- { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     -- { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>",     mode = { "c" },      function() require("flash").toggle() end,     desc = "Toggle Flash Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }

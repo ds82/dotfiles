49519c857e726b469cd7db26ea0ef8a9285efd3c
Dennis Sänger <git@mail.ds82.de>
2024-01-25 19:32:15 +0100

    fix(nvim/nvim-cmp): add ESC to compare->abort keys

--- nvim/lua/dennis-io/plugins/nvim-cmp.lua
+++ nvim/lua/dennis-io/plugins/nvim-cmp.lua
@@ -34,6 +34,7 @@ return {
 				["<C-[>"] = cmp.mapping.scroll_docs(-4),
 				["<C-]>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
+				["<ESC>"] = cmp.mapping.abort(), -- close completion window
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({
 					behavior = cmp.ConfirmBehavior.Insert,

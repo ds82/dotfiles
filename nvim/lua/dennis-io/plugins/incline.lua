6c6b2714d1e9aab7277af0aa01a86851c02800c6
Dennis Sänger <git@mail.ds82.de>
2024-02-25 21:59:48 +0100

    feat(nvim): configure incline to highlight unsaved changes

--- nvim/lua/dennis-io/plugins/incline.lua
+++ nvim/lua/dennis-io/plugins/incline.lua
@@ -18,13 +18,23 @@ return {
 				cursorline = true,
 			},
 			render = function(props)
+				local helpers = require("incline.helpers")
+
 				local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+				local filename_color = colors.text
+				local icon, icon_color = require("nvim-web-devicons").get_icon_color(filename)
+				local guibg = colors.crust
+
 				if vim.bo[props.buf].modified then
 					filename = "[+] " .. filename
+					-- guibg = colors.peach
+					filename_color = colors.peach
 				end
 
-				local icon, color = require("nvim-web-devicons").get_icon_color(filename)
-				return { { icon, guifg = color }, { " " }, { filename } }
+				return {
+					{ icon, guifg = icon_color, guibg = guibg },
+					{ filename, guifg = filename_color, guibg = guibg },
+				}
 			end,
 		})
 	end,

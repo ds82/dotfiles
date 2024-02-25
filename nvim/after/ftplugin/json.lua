285ffcc470135fb8152727efca74ad8b3f227bee
Dennis Sänger <git@mail.ds82.de>
2024-02-25 21:58:18 +0100

    fix(nvim): open json files unfolded

--- nvim/after/ftplugin/json.lua
+++ nvim/after/ftplugin/json.lua
@@ -1,3 +1,5 @@
 vim.o.foldmethod = "expr"
 vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-vim.o.foldlevelstart = 99 -- open all folds when opening file
+vim.o.foldlevel = 9999 -- open all folds when opening file
+vim.o.foldlevelstart = 9999 -- open all folds when opening file
+vim.o.conceallevel = 0

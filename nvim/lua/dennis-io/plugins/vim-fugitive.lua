82ec7168c89ded33d1340b12377ed93ad77251e5
Dennis Sänger <git@mail.ds82.de>
2024-04-01 20:57:54 +0200

    feat(nvim): disable fugitive

--- nvim/lua/dennis-io/plugins/vim-fugitive.lua
+++ nvim/lua/dennis-io/plugins/vim-fugitive.lua
@@ -1,10 +1,11 @@
-return {
-	"tpope/vim-fugitive",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-		vim.keymap.set("n", "<leader>grc", function()
-			vim.cmd(":G rebase --continue")
-		end)
-	end,
-}
+return {}
+-- return {
+-- 	"tpope/vim-fugitive",
+-- 	event = { "BufReadPre", "BufNewFile" },
+-- 	config = function()
+-- 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+-- 		vim.keymap.set("n", "<leader>grc", function()
+-- 			vim.cmd(":G rebase --continue")
+-- 		end)
+-- 	end,
+-- }

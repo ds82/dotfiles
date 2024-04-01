aa2e444c5a307717c89b4ac3777790f4c34152e0
Dennis Sänger <git@mail.ds82.de>
2024-04-01 20:57:28 +0200

    feat(nvim): add zen mode + keybinds

--- nvim/lua/dennis-io/plugins/zen-mode.lua
+++ nvim/lua/dennis-io/plugins/zen-mode.lua
@@ -1,4 +1,18 @@
 return {
 	"folke/zen-mode.nvim",
-	opts = {},
+	config = function()
+		require("zen-mode").setup({
+			window = {
+				width = 1.0,
+			},
+			kitty = {
+				enabled = false,
+				font = "+4", -- font size increment
+			},
+		})
+
+		vim.keymap.set("n", "<leader>z", function()
+			require("zen-mode").toggle()
+		end)
+	end,
 }

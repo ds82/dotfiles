780e016b345afcccf5681d05205ede195792591f
Dennis Sänger <git@mail.ds82.de>
2024-04-06 14:45:28 +0200

    feat(nvim): update neogit config

--- nvim/lua/dennis-io/plugins/neogit.lua
+++ nvim/lua/dennis-io/plugins/neogit.lua
@@ -15,19 +15,41 @@ return {
 			neogit.action("log", "log_current", {})()
 		end, {})
 
+		neogit.setup()
+
 		local keymap = vim.keymap
+		-- keymap.set("n", "<leader><leader>g", function()
+		-- 	neogit.open({ kind = "replace" })
+		-- end, { desc = "Open Neogit Status", silent = true }) -- mapping to restart lsp if necessary
+
 		keymap.set("n", "<leader><leader>g", function()
-			neogit.open({ kind = "replace" })
-		end, { desc = "Open Neogit Status", silent = true }) -- mapping to restart lsp if necessary
+			local file_dir = vim.fn.expand("%:h")
+			print("file_dir: " .. file_dir)
+
+			local exec_result = vim.system({ "git", "rev-parse", "--show-toplevel" }, { text = true, cwd = file_dir })
+				:wait()
+			local root = vim.fn.trim(exec_result.stdout)
+
+			neogit.open({ kind = "replace", cwd = root })
+		end, { desc = "Neogit status", silent = true })
 
 		keymap.set("n", "<leader><leader>l", function()
 			neogit.action("log", "log_current", {})()
-		end, { desc = "Open Git History", silent = true }) -- mapping to restart lsp if necessary
+		end, { desc = "Open Git Log", silent = true }) -- mapping to restart lsp if necessary
 
 		keymap.set("n", "<leader><leader>w", function()
 			vim.cmd(":silent !git add %")
 		end, { desc = "Stage current file in git", silent = true }) -- mapping to restart lsp if necessary
 
-		neogit.setup()
+		keymap.set("n", "<leader><leader>m", function()
+			local file_dir = vim.fn.expand("%:h")
+			print("file_dir: " .. file_dir)
+
+			local exec_result = vim.system({ "git", "rev-parse", "--show-toplevel" }, { text = true, cwd = file_dir })
+				:wait()
+			local root = vim.fn.trim(exec_result.stdout)
+
+			neogit.open({ kind = "replace", cwd = root })
+		end, { desc = "Neogit status", silent = true })
 	end,
 }

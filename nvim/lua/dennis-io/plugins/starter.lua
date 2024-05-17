172d841a668cee1a21b806e903da174a3ded0b23
Dennis Sänger <git@mail.ds82.de>
2024-05-17 12:54:44 +0200

    feat(nvim): restore session from starter

--- nvim/lua/dennis-io/plugins/starter.lua
+++ nvim/lua/dennis-io/plugins/starter.lua
@@ -1,17 +1,26 @@
 return {
-  "echasnovski/mini.starter",
-  -- event = "VimEnter",
-  config = function()
-    require('mini.starter').setup({
-      header = table.concat({
-        [[  /\ \▔\___  ___/\   /(●)_ __ ___  ]],
-        [[ /  \/ / _ \/ _ \ \ / / | '_ ` _ \ ]],
-        [[/ /\  /  __/ (_) \ V /| | | | | | |]],
-        [[\_\ \/ \___|\___/ \_/ |_|_| |_| |_|]],
-        [[───────────────────────────────────]],
-      }, "\n"),
+	"echasnovski/mini.starter",
+	-- event = "VimEnter",
+	config = function()
+		local starter = require("mini.starter")
+		starter.setup({
+			header = table.concat({
+				[[  /\ \▔\___  ___/\   /(●)_ __ ___  ]],
+				[[ /  \/ / _ \/ _ \ \ / / | '_ ` _ \ ]],
+				[[/ /\  /  __/ (_) \ V /| | | | | | |]],
+				[[\_\ \/ \___|\___/ \_/ |_|_| |_| |_|]],
+				[[───────────────────────────────────]],
+			}, "\n"),
 
-    })
-  end,
+			items = {
+				{
+					name = "Restore Session",
+					action = "lua require('persistence').load()",
+					section = "Session",
+				},
+				starter.sections.recent_files(10, true),
+				starter.sections.builtin_actions(),
+			},
+		})
+	end,
 }
-

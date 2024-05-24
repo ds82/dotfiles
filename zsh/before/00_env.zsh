35b328266377bc1ea54b57d38daf3fa45012b584
Dennis Sänger <git@mail.ds82.de>
2024-05-24 22:15:45 +0200

    feat(zsh): add ~/.local/bin to PATH

--- zsh/before/00_env.zsh
+++ zsh/before/00_env.zsh
@@ -3,3 +3,9 @@ export FW=$HOME/code/firmare
 
 # force ESLINT_D (nvim) to use local eslint
 export ESLINT_D_LOCAL_ESLINT_ONLY=1
+
+# add ~/.local/bin to PATH
+LOCAL_BIN=$HOME/.local/bin
+if [[ -d $LOCAL_BIN ]]; then
+  export PATH=$LOCAL_BIN:$PATH
+fi

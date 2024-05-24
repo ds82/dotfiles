4c86e0fc2c3a0f1c3e56a5f14bbea601a20a94f2
Dennis Sänger <git@mail.ds82.de>
2024-05-24 22:02:35 +0200

    feat(nvim): improve rustaceanvim config

--- nvim/lua/dennis-io/plugins/rustaceanvim.lua
+++ nvim/lua/dennis-io/plugins/rustaceanvim.lua
@@ -1,24 +1,29 @@
 local on_attach = require("dennis-io.core.lsp.on_attach")
+vim.g.rustaceanvim = {
+
+	---@type RustaceanToolsOpts
+	tools = {
+		auto_focus = true,
+	},
+	---@type RustaceanLspClientOpts
+	server = {
+		on_attach = function(client, bufnr)
+			vim.lsp.inlay_hint.enable(bufnr, false)
+			vim.lsp.inlay_hint.enable(bufnr, true)
+			on_attach(client, bufnr)
+		end,
+		default_settings = {
+			-- 	-- rust-analyzer language server configuration
+			["rust-analyzer"] = {},
+		},
+	},
+	---@type RustaceanDapOpts
+	dap = {},
+}
 
 return {
 	"mrcjkb/rustaceanvim",
 	version = "^4", -- Recommended
 	lazy = false, -- This plugin is already lazy
-	config = function()
-		vim.g.rustaceanvim = {
-			---@type RustaceanToolsOpts
-			tools = {},
-			---@type RustaceanLspClientOpts
-			server = {
-				on_attach = on_attach,
-				-- default_settings = {
-				-- 	-- rust-analyzer language server configuration
-				-- 	["rust-analyzer"] = {},
-				-- },
-			},
-			-- ---@type RustaceanDapOpts
-			-- dap = {
-			-- },
-		}
-	end,
+	config = function() end,
 }

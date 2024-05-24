8a3ad0b46814d4d3fc4a3e92d0985fe6d4c9c13c
Dennis Sänger <git@mail.ds82.de>
2024-05-24 21:58:56 +0200

    feat(zsh): improve fixssh function

--- zsh/after/fixssh.zsh
+++ zsh/after/fixssh.zsh
@@ -3,10 +3,18 @@ addssh() {
 }
 
 fixssh() {
-  for key in SSH_AUTH_SOCK SSH_CONNECTION SSH_CLIENT; do
-    if (tmux show-environment | grep "^${key}" > /dev/null); then
-      value=`tmux show-environment | grep "^${key}" | sed -e "s/^[A-Z_]*=//"`
-      export ${key}="${value}"
-    fi
-  done
+  SOCK=$(fd -p '.*ssh-.*/agent.*' /var/ /tmp/)
+  if [[ ! -z "${SOCK}" ]]; then
+    export SSH_AUTH_SOCK="$SOCK" 
+    # echo "set SSH_AUTH_SOCK=${SOCK}"
+  fi
+
+  # for key in SSH_AUTH_SOCK SSH_CONNECTION SSH_CLIENT; do
+  #   if (tmux show-environment | grep "^${key}" > /dev/null); then
+  #     value=`tmux show-environment | grep "^${key}" | sed -e "s/^[A-Z_]*=//"`
+  #     export ${key}="${value}"
+  #   fi
+  # done
 }
+
+fixssh

c518b9c2647e540aee245c7edbcd4addea52202b
Dennis Sänger <git@mail.ds82.de>
2024-07-03 08:32:00 +0200

    feat(zsh): addd fzf-homebrew-outdated fn

--- zsh/after/fzf.zsh
+++ zsh/after/fzf.zsh
@@ -5,3 +5,6 @@ export FZF_DEFAULT_OPTS="--history=$HOME/.fzf_history"
 [[ -f "$HOME/.fzf.zsh" ]] && source "$HOME/.fzf.zsh"
 
 
+function fzf-homebrew-update() {
+  brew install $(brew outdated | fzf --multi)
+}

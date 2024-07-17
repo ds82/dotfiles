9604f27baf76c0c234f017e7087653ee943b03b0
Dennis Sänger <git@mail.ds82.de>
2024-07-17 07:23:42 +0200

    feat(nvim): add cpp specific config (ftplugin)

--- nvim/after/ftplugin/cpp.lua
+++ nvim/after/ftplugin/cpp.lua
@@ -1,1 +1,16 @@
-vim.keymap.set("n", "<leader>6", ":ClangdSwitchSourceHeader<CR>")
+vim.o.wrap = false
+vim.o.linebreak = false
+
+vim.bo.tabstop = 2 -- size of a hard tabstop (ts).
+vim.bo.shiftwidth = 2 -- size of an indentation (sw).
+
+vim.keymap.set("n", "<leader>6", function()
+	vim.cmd("ClangdSwitchSourceHeader")
+end)
+
+vim.keymap.set("n", "<leader><leader>6", function()
+	local filename_wo_extension = vim.fn.expand("%:t:r")
+	local builtin = require("telescope.builtin")
+
+	builtin.find_files({ default_text = filename_wo_extension })
+end)

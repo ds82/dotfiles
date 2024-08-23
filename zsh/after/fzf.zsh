5d2e672ac14d894b8759880c36e9547016cef7b5
Dennis Sänger <git@mail.ds82.de>
2024-08-23 22:38:06 +0200

    feat(zsh): add fzf-glab-mr-list function

--- zsh/after/fzf.zsh
+++ zsh/after/fzf.zsh
@@ -9,6 +9,15 @@ function fzf-homebrew-update() {
   brew install $(brew outdated | fzf --multi)
 }
 
+function fzf-glab-mr-list {
+  glab mr list \
+      | awk -F\t 'NR>2 {printf "\033[1;32m%s\t\033[00m\033[1;33m%s\t\033[00m\033[1;34m%s\033[00m\t\033[00m\033[1;36m%s\033[00m\n", $1, $2, $3, $4;}' \
+      | column -s$'\t' -t \
+      | fzf --color --ansi \
+      | awk '{print $1}' \
+      | sed -E 's/!([0-9]+)/\1/'
+}
+
 function fzf-glab-mr-co() {
   glab mr checkout $(glab mr list \
     | awk -F\t 'NR>2 {printf "\033[1;32m%s\t\033[00m\033[1;33m%s\t\033[00m\033[1;34m%s\033[00m\t\033[00m\033[1;36m%s\033[00m\n", $1, $2, $3, $4;}' \

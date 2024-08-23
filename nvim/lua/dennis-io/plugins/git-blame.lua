fb97594212cc0e9c09167e41c43f653fed5d112d
Dennis Sänger <git@mail.ds82.de>
2024-08-23 21:46:15 +0200

    feat(nvim): show sha in git-blame line

--- nvim/lua/dennis-io/plugins/git-blame.lua
+++ nvim/lua/dennis-io/plugins/git-blame.lua
@@ -1,4 +1,7 @@
 return {
-  "f-person/git-blame.nvim",
-  event = "VeryLazy"
+	"f-person/git-blame.nvim",
+	init = function()
+		vim.g.gitblame_message_template = "  <author> • <date> • <summary> • <sha>"
+	end,
+	event = "VeryLazy",
 }

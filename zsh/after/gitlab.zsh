13c53f23fc6226014a3684aac6354d8a579d450b
Dennis Sänger <git@mail.ds82.de>
2024-08-23 22:39:01 +0200

    feat(zsh): make review function work w/o MR number

--- zsh/after/gitlab.zsh
+++ zsh/after/gitlab.zsh
@@ -1,6 +1,12 @@
+
 review() {
-  T=`mktemp /tmp/glab.XXXXXX`
   MR=$1
+
+  if [[ ! -n "$1" ]]; then
+    MR=$(fzf-glab-mr-list)
+  fi
+
+  T=`mktemp /tmp/glab.XXXXXX`
   glab api /projects/:id/merge_requests/$MR >> $T
   SHA=$(jq -r '.sha' $T)
   TARGET=$(jq -r '.target_branch' $T)

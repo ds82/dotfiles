6471619532336e11cced8a9fc76c188cfb6a5652
Dennis Sänger <git@mail.ds82.de>
2024-08-23 22:10:49 +0200

    feat(nvim): add bullets-vim/bullets.vim plugin

---
+++ nvim/lua/dennis-io/plugins/bullets.lua
@@ -0,0 +1,12 @@
+return {
+	"bullets-vim/bullets.vim",
+	setup = function()
+		vim.g.bullets_enabled_file_types = {
+			"markdown",
+			"text",
+			"gitcommit",
+			"gitrebase",
+			"gitconfig",
+		}
+	end,
+}

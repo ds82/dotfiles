43d8fae28d5e53f789118b376bceddfd48a3e1ef
Dennis Sänger <git@mail.ds82.de>
2024-09-03 07:36:41 +0200

    feat(bin): add remap-keys.sh script
    This should be integrated into install.yml somehow

---
+++ bin/remap-keys.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# https://gist.github.com/bennlee/0f5bc8dc15a53b2cc1c81cd92363bf18?permalink_comment_id=4791480
+#
+
+# remap caps-lock to F12
+hidutil property --set '{"UserKeyMapping":[
+  {
+    "HIDKeyboardModifierMappingSrc": 0x700000039,
+    "HIDKeyboardModifierMappingDst": 0x700000045
+  }
+]}'

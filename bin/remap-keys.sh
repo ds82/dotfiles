c1e3c09e13649a4be95f6aedea816f94506f33ba
Dennis Sänger <git@mail.ds82.de>
2024-09-03 19:52:25 +0200

    feat(osx): change caps lock -> F10 mapping via OSX hidutil

--- bin/remap-keys.sh
+++ bin/remap-keys.sh
@@ -8,6 +8,6 @@
 hidutil property --set '{"UserKeyMapping":[
   {
     "HIDKeyboardModifierMappingSrc": 0x700000039,
-    "HIDKeyboardModifierMappingDst": 0x700000045
+    "HIDKeyboardModifierMappingDst": 0x700000043
   }
 ]}'

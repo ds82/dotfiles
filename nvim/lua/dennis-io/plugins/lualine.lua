b7a58827a04ab8e8c32ba92d1750f76b6e760b87
Dennis Sänger <git@mail.ds82.de>
2024-09-04 21:16:30 +0200

    feat(nvim): add pretty git stats to lualine

--- nvim/lua/dennis-io/plugins/lualine.lua
+++ nvim/lua/dennis-io/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local lualine = require("lualine")
 		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-		-- local mocha = require("catppuccin.palettes").get_palette("mocha")
+		local mocha = require("catppuccin.palettes").get_palette("mocha")
 
 		-- configure lualine with modified theme
 		lualine.setup({
@@ -13,6 +13,19 @@ return {
 			},
 			sections = {
 				lualine_a = { "mode" },
+				lualine_b = {
+					"branch",
+					{
+						"diff",
+						-- Is it me or the symbol for modified us really weird
+						symbols = { added = " ", modified = "󰝤 ", removed = " " },
+						diff_color = {
+							added = { fg = mocha.green },
+							modified = { fg = mocha.peach },
+							removed = { fg = mocha.red },
+						},
+					},
+				},
 				lualine_c = {
 					{
 						require("noice").api.statusline.mode.get,

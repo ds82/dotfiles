86c0a5bdb273b62ac8c9642782aa3e7dc74ff419
Dennis Sänger <git@mail.ds82.de>
2024-09-14 19:53:41 +0200

    feat: move remap-keys to autoexec/

--- autoexec/remap-keys.sh
+++ autoexec/remap-keys.sh
@@ -4,7 +4,7 @@
 # https://gist.github.com/bennlee/0f5bc8dc15a53b2cc1c81cd92363bf18?permalink_comment_id=4791480
 #
 
-# remap caps-lock to F12
+# remap caps-lock to F10
 hidutil property --set '{"UserKeyMapping":[
   {
     "HIDKeyboardModifierMappingSrc": 0x700000039,

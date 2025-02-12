7251f4815dc6f0b1a0e4330da450fa8f6455a2a2
Dennis Sänger <git@mail.ds82.de>
2025-02-12 17:31:59 +0100

    fix(zsh): try to grep colors better

--- zsh/after/colors.zsh
+++ zsh/after/colors.zsh
@@ -1,2 +1,5 @@
 # export GREP_COLORS="1;33;100"
 export GREP_COLORS="sl=97;48;5;236:cx=37;40:mt=30;48;5;186:fn=38;5;197:ln=38;5;154:bn=38;5;141:se=38;5;81"
+export GREP_COLOR="30;48;5;186"
+
+export FORCE_COLOR=true

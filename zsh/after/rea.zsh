5ba2b7914be5968e3ace4e771f22c09afb00f04a
Dennis Sänger <git@mail.ds82.de>
2025-04-04 15:14:27 +0200

    feat(zsh): improved updateDevice function

--- zsh/after/rea.zsh
+++ zsh/after/rea.zsh
@@ -2,6 +2,21 @@ updateDevice() {
   IP=$1
   FILE=$2
   REBOOT=${3:-true}
+
+  # check if we can login without password
+  ssh -o PasswordAuthentication=no drkopf@$IP /bin/true &>/dev/null
+
+  if [ $? -ne 0 ]; then
+    echo "Could not login without password - using ssh-copy-id" 
+    ssh-copy-id drkopf@$IP
+
+    if [ $? -ne 0 ]; then
+      echo "Could not login without password - exiting"
+      return 1
+    fi
+  fi
+
+
   scp $FILE drkopf@$IP:/usr/reajet/public/service/update.hru
   SCP_SUCCESS=$0
   [[ "$SCP_SUCCESS" -eq 0 && $REBOOT != "false" ]] && ssh drkopf@$IP 'sudo reboot'
@@ -12,5 +27,6 @@ downloadFirmware() {
   rsync -avP re-devsrv01:/mnt/inchgower_public/firmware/by-build-number/$BUILD/*$BUILD*.hru ~/Downloads/
   
 }
+
 
 alias git-me-rea='git config user.name "Dennis Sänger" ; git config user.email "dsaenger@rea.de"'

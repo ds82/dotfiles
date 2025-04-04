1476e0350a52c8b3259ec0a052b5608e32c6b0c0
Dennis Sänger <git@mail.ds82.de>
2025-04-04 15:15:15 +0200

    feat(zsh): improve review function

--- zsh/after/gitlab.zsh
+++ zsh/after/gitlab.zsh
@@ -6,10 +6,14 @@ review() {
     MR=$(fzf-glab-mr-list)
   fi
 
+  git fetch --all
+
   T=`mktemp /tmp/glab.XXXXXX`
   glab api /projects/:id/merge_requests/$MR >> $T
   SHA=$(jq -r '.sha' $T)
   TARGET=$(jq -r '.target_branch' $T)
+  SOURCE=$(jq -r '.source_branch' $T)
   echo "compare origin/$TARGET..$SHA"
+  git switch $SOURCE && git rho
   nvim +"DiffviewOpen origin/$TARGET..$SHA"
 }

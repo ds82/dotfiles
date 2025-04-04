530bed853285b2f8adc95bc8978b0a1a396d18b0
Dennis Sänger <git@mail.ds82.de>
2025-04-04 15:13:37 +0200

    feat(zsh): download firmware build by build no

--- zsh/after/rea.zsh
+++ zsh/after/rea.zsh
@@ -7,4 +7,10 @@ updateDevice() {
   [[ "$SCP_SUCCESS" -eq 0 && $REBOOT != "false" ]] && ssh drkopf@$IP 'sudo reboot'
 }
 
+downloadFirmware() {
+  BUILD=$1
+  rsync -avP re-devsrv01:/mnt/inchgower_public/firmware/by-build-number/$BUILD/*$BUILD*.hru ~/Downloads/
+  
+}
+
 alias git-me-rea='git config user.name "Dennis Sänger" ; git config user.email "dsaenger@rea.de"'
